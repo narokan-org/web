37bbe72e1b0cd384feb24629729ae57cd8e64996
uioporqwerty <nitish.sachar@protonmail.com>
2024-07-28 16:39:26 -0700

    Configure tailwind theme and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.svelte'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 	plugins: []
 };
