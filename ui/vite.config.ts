84ea8c770af6bdc66862cd66ebe31a4589d1ef9c
uioporqwerty <nitish.sachar@protonmail.com>
2024-07-28 11:00:39 -0700

    clean up

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	build: {
+		outDir: 'dist'
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
