afef4cc9576c24906dd0a21f15abd101d1484aa1
uioporqwerty <nitish.sachar@protonmail.com>
2024-07-28 19:28:22 -0700

    Add header

---
+++ src/lib/components/header/header.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { page } from '$app/stores';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	$: activeUrl = $page.url.pathname;
+</script>
+
+<Navbar>
+	<NavBrand href="/">
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Basel</span>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl {activeUrl}>
+		<NavLi href="/login">Login</NavLi>
+	</NavUl>
+</Navbar>
