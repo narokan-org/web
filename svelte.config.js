529f6a48565b20359469d32bedfa565bfc42616e
uioporqwerty <nitish.sachar@protonmail.com>
2024-07-29 06:49:53 -0700

    test

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,29 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: azure()
+		adapter: azure({
+			customStaticWebAppConfig: {
+				routes: [
+					{
+						route: '/login',
+						rewrite: '/.auth/login/aad',
+						allowedRoles: ['anonymous', 'authenticated']
+					},
+					{
+						route: '/.auth/login/github',
+						statusCode: 404
+					},
+					{
+						route: '/.auth/login/twitter',
+						statusCode: 404
+					},
+					{
+						route: '/*',
+						allowedRoles: ['anonymous', 'authenticated']
+					}
+				]
+			}
+		})
 	}
 };
 
