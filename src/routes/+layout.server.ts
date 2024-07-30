bac1ea2cdd676e8285714eaa8391298cb9ffa6a1
uioporqwerty <nitish.sachar@protonmail.com>
2024-07-29 19:12:49 -0700

    test

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -10,10 +10,12 @@ export interface JwtPayload {
 }
 
 export const load = async ({ request, cookies }) => {
-	// For production, x-ms-client-principal is passed to the server in the request headers. For local development that is not the case so we decode the cookie directly. This is a limitation of swa cli currently
+	// For production, x-ms-client-principal is passed to the server in the request headers. For local development that is not the case so we decode the cookie directly. This is a limitation of swa cli currently.
+	console.log('Entering load function in +layout.server.ts');
 	const user = isProduction ? decodeByHeader(request) : decodeByCookie(cookies);
 
 	if (!user) {
+		console.error('Could not find user.');
 		fail(500);
 		return;
 	}
@@ -25,12 +27,13 @@ function decodeByHeader(request: Request) {
 	const header = request.headers.get('x-ms-client-principal');
 
 	if (!header) {
-		console.error('No header found');
 		return null;
 	}
 
 	const encoded = Buffer.from(header!, 'base64');
+	console.log('Decoding header: ' + encoded.toString('ascii'));
 	const decoded: JwtPayload = JSON.parse(encoded.toString('ascii'));
+	console.log('Decoded header: ' + decoded);
 
 	return decoded;
 }
