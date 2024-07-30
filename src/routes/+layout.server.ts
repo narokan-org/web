d83773dca45ef896527728f05905af907bcad245
uioporqwerty <nitish.sachar@protonmail.com>
2024-07-29 18:17:59 -0700

    clean up auth response

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,24 +1,48 @@
-import { fail } from '@sveltejs/kit';
+import { isProduction } from '$lib/utils/utils.js';
+import { fail, type Cookies } from '@sveltejs/kit';
+import { jwtDecode } from 'jwt-decode';
+
+export interface JwtPayload {
+	identityProvider: string;
+	userId: string;
+	userDetails: string;
+	userRoles: string[];
+}
 
 export const load = async ({ request, cookies }) => {
-	console.log('load for auth');
-	const header = request.headers.get('x-ms-client-principal');
+	const user = isProduction ? decodeByHeader(request) : decodeByCookie(cookies);
 
-	if (!header) {
-		console.error('No header found');
+	if (!user) {
 		fail(500);
 		return;
 	}
 
-	const encoded = Buffer.from(header!, 'base64');
-	const decoded: {
-		identityProvider: string;
-		userId: string;
-		userDetails: string;
-		userRoles: string[];
-	} = JSON.parse(encoded.toString('ascii'));
-
-	const isLoggedIn = decoded !== null;
-	console.log(`returning: ${JSON.stringify({ isLoggedIn, decoded })}`);
-	return { isLoggedIn, decoded };
+	return { isLoggedIn: true, user };
 };
+
+function decodeByHeader(request: Request) {
+	const header = request.headers.get('x-ms-client-principal');
+
+	if (!header) {
+		console.error('No header found');
+		return null;
+	}
+
+	const encoded = Buffer.from(header!, 'base64');
+	const decoded: JwtPayload = JSON.parse(encoded.toString('ascii'));
+
+	return decoded;
+}
+
+function decodeByCookie(cookies: Cookies) {
+	const token = cookies.get('StaticWebAppsAuthCookie');
+
+	if (!token) {
+		console.error('No token found');
+		return null;
+	}
+
+	let decoded: JwtPayload = jwtDecode(token, { header: true });
+
+	return decoded;
+}
