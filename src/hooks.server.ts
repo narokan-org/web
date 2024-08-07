dc9e767e3737ec1b64da419441b49a81a04185f3
uioporqwerty <nitish.sachar@protonmail.com>
2024-08-06 17:19:01 -0700

    Add protection for secure routes.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,4 @@
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 import { UserService } from '$lib/services/user-service';
 import { TelemetryService } from '$lib/services/telemetry-service';
 import { LoggingService } from '$lib/services/logging-service';
@@ -7,6 +7,24 @@ export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.loggingService = new LoggingService();
 	event.locals.telemetryService = new TelemetryService(event.locals.loggingService);
 	event.locals.userService = new UserService(event.fetch, event.locals.loggingService);
+
+	const protectedRoutes = [
+		'/assessments',
+		'/controls',
+		'/dashboard',
+		'/entities',
+		'/logout',
+		'/risks',
+		'/settings'
+	];
+
+	if (protectedRoutes.includes(event.url.pathname)) {
+		const user = await event.locals.userService.getUser();
+
+		if (!user) {
+			throw redirect(302, '/login');
+		}
+	}
 
 	const response = await resolve(event);
 	return response;
