a0bbc38aeff80ee5c418ea1c4b66ad53457b70bd
uioporqwerty <nitish.sachar@protonmail.com>
2024-08-06 19:30:15 -0700

    test hardcode

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -2,10 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export const load = ({ cookies }) => {
 	const storedRedirectUrl = cookies.get('basel-redirect-url') ?? '/';
-	cookies.delete('basel-redirect-url', { path: '/' });
+	// cookies.delete('basel-redirect-url', { path: '/' });
 
-	redirect(
-		301,
-		`/.auth/login/aad?post_login_redirect_uri=${encodeURIComponent(storedRedirectUrl)}`
-	);
+	redirect(301, `/.auth/login/aad?post_login_redirect_uri=${encodeURIComponent('/dashboard')}`);
 };
