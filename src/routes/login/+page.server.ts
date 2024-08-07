1c216d9549ba794f1df5b7a299727b9533407f9e
uioporqwerty <nitish.sachar@protonmail.com>
2024-08-06 19:39:19 -0700

    try no decode

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export const load = ({ cookies }) => {
 	const storedRedirectUrl = cookies.get('basel-redirect-url') ?? '/';
-	// cookies.delete('basel-redirect-url', { path: '/' });
+	cookies.delete('basel-redirect-url', { path: '/' });
 
-	redirect(301, `/.auth/login/aad?post_login_redirect_uri=${encodeURIComponent('/dashboard')}`);
+	redirect(301, `/.auth/login/aad?post_login_redirect_uri=${storedRedirectUrl}`);
 };
