77bcd2f50cbd528f7db40f62189bab3ae3d14c9e
uioporqwerty <nitish.sachar@protonmail.com>
2024-08-08 11:57:12 -0700

    test

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,5 +1,6 @@
+import { isProduction } from '$lib/utils/utils';
 import { redirect } from '@sveltejs/kit';
 
 export const load = () => {
-	redirect(302, `/.auth/login/aad`);
+	redirect(302, `/.auth/login/${isProduction() ? 'aadb2c' : 'aad'}`);
 };
--- svelte.config.js
+++ svelte.config.js
@@ -10,9 +10,32 @@ const config = {
 	kit: {
 		adapter: azure({
 			customStaticWebAppConfig: {
+				auth: {
+					identityProviders: {
+						customOpenIdConnectProviders: {
+							aadb2c: {
+								registration: {
+									clientIdSettingName: 'AADB2C_PROVIDER_CLIENT_ID',
+									clientCredential: {
+										clientSecretSettingName: 'AADB2C_PROVIDER_CLIENT_SECRET'
+									},
+									openIdConnectConfiguration: {
+										wellKnownOpenIdConfiguration:
+											'https://baselapp.b2clogin.com/baselapp.onmicrosoft.com/B2C_1_Basel_Sign_Up/v2.0/.well-known/openid-configuration'
+									}
+								},
+								login: {
+									nameClaimType: 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name',
+									scopes: [],
+									loginParameterNames: []
+								}
+							}
+						}
+					}
+				},
 				responseOverrides: {
 					401: {
-						redirect: '/.auth/login/aad?post_login_redirect_uri=.referrer',
+						redirect: `/.auth/login/${process.env.NODE_ENV === 'production' ? 'aadb2c' : 'aad'}?post_login_redirect_uri=.referrer`,
 						statusCode: 302
 					}
 				},
