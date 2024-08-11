7f822c2e486e54e59dfafb0954ee95a0da549438
uioporqwerty <nitish.sachar@protonmail.com>
2024-08-11 07:24:32 -0700

    Rename to company

---
+++ database/basel/company.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS [dbo].[Company];
+
+CREATE TABLE [dbo].[Company] (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(16) NOT NULL
+);
--- database/basel/workspace.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS [dbo].[Workspace];
-
-CREATE TABLE [dbo].[Workspace] (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
-    Name NVARCHAR(16) NOT NULL
-);
