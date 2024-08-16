b607842bd60b9f7024dc18620ae6383d8268a8e3
uioporqwerty <nitish.sachar@protonmail.com>
2024-08-16 15:59:04 -0700

    Keep calm and commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	// import BrainSolid from 'flowbite-svelte-icons/BrainSolid.svelte';
 	// import LockSolid from 'flowbite-svelte-icons/LockSolid.svelte';
 	// import FileChartBarSolid from 'flowbite-svelte-icons/FileChartBarSolid.svelte';
-	// testing s
+	// testing s s
 	import { t } from '$lib/translations';
 	import Feature from '$lib/components/feature/index.svelte';
 
