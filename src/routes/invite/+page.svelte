31092fdad38d58c5e6e5b87e5f1a8daccf53d7bb
uioporqwerty <nitish.sachar@protonmail.com>
2024-08-16 13:07:34 -0700

    Support for invite page

---
+++ src/routes/invite/+page.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+	import { Button, Heading, P, Input, Hr, ButtonGroup } from 'flowbite-svelte';
+	import PlusOutline from 'flowbite-svelte-icons/PlusOutline.svelte';
+	import { t } from '$lib/translations';
+
+	let emailInputs = [true, true, true, false, false];
+	let addAnotherClickCount = 0;
+
+	function addEmailInput() {
+		if (addAnotherClickCount < 2) {
+			emailInputs[3 + addAnotherClickCount] = true;
+			addAnotherClickCount += 1;
+		}
+	}
+</script>
+
+<div class="flex flex-col items-center justify-center min-h-screen w-full">
+	<div class="min-w-[600px]">
+		<Heading data-testid="invite-heading" class="text-2xl" level={2}
+			>{$t('common.pages.invite.heading')}</Heading
+		>
+
+		<P data-testid="invite-subheading" class="text-lg mt-2"
+			>{$t('common.pages.invite.subheading')}</P
+		>
+
+		<form method="POST">
+			<div class="mt-2">
+				{#each emailInputs as visible, index}
+					{#if visible}
+						<Input
+							id={`email-input-${index}`}
+							class="text-sm mt-2"
+							name={`email-${index}`}
+							type="email"
+							placeholder={$t('common.pages.invite.emailAddressInputPlaceholder')}
+						/>
+					{/if}
+				{/each}
+			</div>
+
+			<div class="flex flex-row mt-2">
+				{#if addAnotherClickCount < 2}
+					<Button
+						data-testid="invite-add-button"
+						class="text-sm border-none flex-1 justify-start no-hover"
+						color="alternative"
+						type="button"
+						on:click={addEmailInput}
+					>
+						<PlusOutline class="w-5 h-5 me-2" />
+						{$t('common.pages.invite.addAnotherButtonLabel')}
+					</Button>
+				{/if}
+
+				<Button class="text-sm ml-auto" data-testid="invite-send-button" type="submit"
+					>{$t('common.pages.invite.sendButtonLabel')}</Button
+				>
+			</div>
+		</form>
+
+		<Hr class="mt-6" />
+
+		<ButtonGroup class="w-full">
+			<Input disabled type="text" value="" />
+			<Button data-testid="invite-copy-button" color="primary"
+				>{$t('common.pages.invite.copyInviteLabel')}</Button
+			>
+		</ButtonGroup>
+
+		<div class="flex justify-center">
+			<Button class="text-sm mt-12 " data-testid="invite-skip-button" href="/dashboard"
+				>{$t('common.pages.invite.skipButtonLabel')}</Button
+			>
+		</div>
+	</div>
+</div>
---
+++ src/routes/invite/page.svelte.test.ts
@@ -0,0 +1,66 @@
+import { describe, it, expect } from 'vitest';
+import { screen, render } from '@testing-library/svelte';
+import userEvent from '@testing-library/user-event';
+import Page from './+page.svelte';
+import common from '$lib/translations/en/common.json';
+
+describe('invite page', () => {
+	it('should render', async () => {
+		render(Page);
+
+		expect(screen.getByTestId('invite-heading')).toHaveTextContent(common.pages.invite.heading);
+		expect(screen.getByTestId('invite-subheading')).toHaveTextContent(
+			common.pages.invite.subheading
+		);
+		expect(
+			screen.getAllByRole('textbox').filter((input) => (input as HTMLInputElement).type === 'email')
+		).toHaveLength(3);
+		expect(screen.getByTestId('invite-send-button')).toHaveTextContent(
+			common.pages.invite.sendButtonLabel
+		);
+		expect(screen.getByTestId('invite-add-button')).toHaveTextContent(
+			common.pages.invite.addAnotherButtonLabel
+		);
+		expect(screen.getByTestId('invite-skip-button')).toHaveTextContent(
+			common.pages.invite.skipButtonLabel
+		);
+		expect(screen.getByTestId('invite-copy-button')).toHaveTextContent(
+			common.pages.invite.copyInviteLabel
+		);
+	});
+
+	it('should add another email input when add button is clicked', async () => {
+		render(Page);
+
+		const addAnotherButton = screen.getByTestId('invite-add-button');
+		await userEvent.click(addAnotherButton);
+
+		expect(
+			screen.getAllByRole('textbox').filter((input) => (input as HTMLInputElement).type === 'email')
+		).toHaveLength(4);
+	});
+
+	it('should add a 5th email input when add button is clicked twice', async () => {
+		render(Page);
+
+		const addAnotherButton = screen.getByTestId('invite-add-button');
+
+		await userEvent.click(addAnotherButton);
+		await userEvent.click(addAnotherButton);
+
+		expect(
+			screen.getAllByRole('textbox').filter((input) => (input as HTMLInputElement).type === 'email')
+		).toHaveLength(5);
+	});
+
+	it('should hide the add button when 5 email inputs are present', async () => {
+		render(Page);
+
+		const addAnotherButton = screen.getByTestId('invite-add-button');
+
+		await userEvent.click(addAnotherButton);
+		await userEvent.click(addAnotherButton);
+
+		expect(addAnotherButton).not.toBeInTheDocument();
+	});
+});
