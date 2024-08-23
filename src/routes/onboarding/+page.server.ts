b7b88bd1a3ea51f24ee378c1dceb646e80c18f37
uioporqwerty <nitish.sachar@protonmail.com>
2024-08-23 16:07:47 -0700

    add logging

--- src/routes/onboarding/+page.server.ts
+++ src/routes/onboarding/+page.server.ts
@@ -3,6 +3,8 @@ import { fail, redirect, type Actions } from '@sveltejs/kit';
 export const actions: Actions = {
 	default: async ({ locals, request }) => {
 		// TODO: Creation of a company should be done as a transaction with a stored procedure using an API function. Since this is a sensitive operation, it should be done server side with rollback capabilities.
+		locals.loggingService.debug('Creating company');
+
 		const formData = await request.formData();
 		const companyName = formData.get('workspace-name') as string;
 
@@ -23,10 +25,12 @@ export const actions: Actions = {
 			return fail(500, { message: 'Failed to get current user' });
 		}
 
+		locals.loggingService.debug('Updating user attributes');
 		await locals.userService.updateUserAttributes(currentUser.id, {
 			extension_Onboarded: true
 		});
 
+		locals.loggingService.debug('Redirecting to invite page');
 		redirect(302, '/invite');
 	}
 };
